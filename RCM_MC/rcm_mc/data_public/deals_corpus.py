5a5a01aa773d5af56e58fe82950cb934c334a98a
DrewThomas09 <ast3801@gmail.com>
2026-04-18 01:45:48 -0500

    auto: wire seed_100 into deals_corpus (1680 deals)

--- RCM_MC/rcm_mc/data_public/deals_corpus.py
+++ RCM_MC/rcm_mc/data_public/deals_corpus.py
@@ -830,6 +830,7 @@ class DealsCorpus:
         from .extended_seed_97 import EXTENDED_SEED_DEALS_97
         from .extended_seed_98 import EXTENDED_SEED_DEALS_98
         from .extended_seed_99 import EXTENDED_SEED_DEALS_99
+        from .extended_seed_100 import EXTENDED_SEED_DEALS_100
         all_seed = (
             _SEED_DEALS + EXTENDED_SEED_DEALS + EXTENDED_SEED_DEALS_2
             + EXTENDED_SEED_DEALS_3 + EXTENDED_SEED_DEALS_4 + EXTENDED_SEED_DEALS_5
@@ -902,6 +903,7 @@ class DealsCorpus:
             + EXTENDED_SEED_DEALS_97
             + EXTENDED_SEED_DEALS_98
             + EXTENDED_SEED_DEALS_99
+            + EXTENDED_SEED_DEALS_100
         )
 
         if skip_if_populated:
